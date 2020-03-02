20a42dec459db14c45ce22ef9128e9bb9a5ddf99
Nicolas Wagner <lenicolaswagner@gmail.com>
2020-03-02 14:13:03 +0100

    first commit

---
+++ FBAT.py
@@ -0,0 +1,213 @@
+import time as times
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+class FBAT:
+	
+	data_train_ok = None
+	models_train_ok = None
+	dist_train_ok = None
+	
+	data_train_ko = None
+	models_train_ko = None
+	dist_train_ko = None
+	
+	data_test_ok = None
+	models_test_ok = None
+	dist_test_ok = None
+	
+	data_test_ko = None
+	models_test_ko = None
+	dist_test_ko = None
+	
+	threshold = None
+	timeTrain = 0
+	timeTest = 0
+	
+	def __init__(self):
+		pass;
+		
+	def set_train(self,ok,ko):
+		self.data_train_ok = [a for a in ok]
+		self.data_train_ko = [a for a in ko]
+		
+	def set_test(self,ok,ko):
+		self.data_test_ok = [a for a in ok]
+		self.data_test_ko = [a for a in ko]
+		
+	@staticmethod
+	def compute_harmonics(TS):
+		take = int(len(TS)/2) +1 
+		Y = np.fft.fft(TS)
+		return Y[:take]
+	
+	@staticmethod	
+	def compute_model(HAR,T,z=1,delay=0):
+		nbOfHarmonics = len(HAR) # harmonic 0 included
+		harmonics = HAR[:(z+1)]
+		freqs = range(z+1)
+		out = []
+		for i in T:
+			time = 0
+			for j in range(len(harmonics)):
+				if j==0:
+					time = time + (abs(harmonics[j])/24.)*np.cos(2*np.pi*i*freqs[j] + np.angle(harmonics[j])- delay)
+				elif j==(nbOfHarmonics-1):
+					time = time + (abs(harmonics[j])/24.)*np.cos(2*np.pi*i*freqs[j] + np.angle(harmonics[j]) - delay)
+				else:
+					time = time + (abs(harmonics[j])/12.)*np.cos(2*np.pi*i*freqs[j] + np.angle(harmonics[j]) - delay)
+			out.append(time)
+		return [a for a in out]
+		
+	@staticmethod	
+	def computeModels(TS,T,p=24,q=12,z=1):
+		TS1 = TS[:p]
+		TS2 = TS[q:q+p]
+		
+		HAR1 = FBAT.compute_harmonics(TS1)
+		HAR2 = FBAT.compute_harmonics(TS2)
+		
+		MOD1 = FBAT.compute_model(HAR1,T,z,0)
+		MOD2 = FBAT.compute_model(HAR2,T,z,(float(q)/float(p))*2*np.pi)
+		
+		return MOD1,MOD2
+		
+	def get_train_performance(self,threshold):
+		nbOK = len(self.dist_train_ok)
+		nbKO = len(self.dist_train_ko)
+		y_pred = []
+		y_true = []
+		
+		for e in self.dist_train_ok:
+			y_true.append(0)
+			if e>threshold:
+				y_pred.append(1)
+			else:
+				y_pred.append(0)
+		for e in self.dist_train_ko:
+			y_true.append(1)
+			if e>threshold:
+				y_pred.append(1)
+			else:
+				y_pred.append(0)
+				
+		accG = accuracy_score(y_true,y_pred)
+		precG = precision_score(y_true,y_pred,average='macro')
+		recG = recall_score(y_true,y_pred,average='macro')
+		labels = [0,1]	
+		prec = precision_score(y_true,y_pred,average=None,labels=labels)
+		rec = recall_score(y_true,y_pred,average=None,labels=labels)	
+		
+		return accG,precG,recG,prec[0],rec[0],prec[1],rec[1]
+		
+	def get_test_performance(self):
+		nbOK = len(self.dist_test_ok)
+		nbKO = len(self.dist_test_ko)
+		y_pred = []
+		y_true = []
+		
+		for e in self.dist_test_ok:
+			y_true.append(0)
+			if e>self.threshold:
+				y_pred.append(1)
+			else:
+				y_pred.append(0)
+		for e in self.dist_test_ko:
+			y_true.append(1)
+			if e>self.threshold:
+				y_pred.append(1)
+			else:
+				y_pred.append(0)
+				
+		accG = accuracy_score(y_true,y_pred)
+		precG = precision_score(y_true,y_pred,average='macro')
+		recG = recall_score(y_true,y_pred,average='macro')
+		labels = [0,1]	
+		prec = precision_score(y_true,y_pred,average=None,labels=labels)
+		rec = recall_score(y_true,y_pred,average=None,labels=labels)	
+		
+		return accG,precG,recG,prec[0],rec[0],prec[1],rec[1]
+		
+	def train_threshold(self):
+	
+		mini = min([min(self.dist_train_ok),min(self.dist_train_ko)])
+		maxi = max([max(self.dist_train_ok),max(self.dist_train_ko)])
+
+		thresholds = np.linspace(mini,maxi,10000)
+
+		bestThres = 0
+		bestInd = 0
+
+		for i in range(len(thresholds)):
+			sc = self.get_train_performance(thresholds[i])[2] ##rec
+			if sc > bestThres:
+				bestThres = sc
+				bestInd = i
+
+		self.threshold = thresholds[bestInd]
+		
+	def fit(self,p=24,q=12,z=1,threshold=None):
+		start = times.time()
+		
+		T = np.linspace(0,1,p)
+			
+		self.models_train_ok = []
+		self.dist_train_ok = []
+		self.models_train_ko = []
+		self.dist_train_ko = []
+		
+		for ts in self.data_train_ok:
+			MOD1,MOD2 = FBAT.computeModels(ts,T,p,q,z)
+			self.models_train_ok.append([MOD1,MOD2])
+			distance = sum([(MOD2[i]-MOD1[i])**2 for i in range(len(MOD1))])**(0.5)
+			self.dist_train_ok.append(distance)
+
+		for ts in self.data_train_ko:
+			MOD1,MOD2 = FBAT.computeModels(ts,T,p,q,z)
+			self.models_train_ko.append([MOD1,MOD2])
+			distance = sum([(MOD2[i]-MOD1[i])**2 for i in range(len(MOD1))])**(0.5)
+			self.dist_train_ko.append(distance)
+		
+		if threshold:
+			self.threshold == threshold
+		else:	
+			self.train_threshold()
+			
+			
+		stop = times.time()
+		self.timeTrain = stop-start
+		
+	
+	def eval_test(self,p=24,q=12,z=1,threshold=None):
+		start = times.time()
+		
+		if threshold:
+			self.threshold = threshold
+		
+		T = np.linspace(0,1,p)
+			
+		self.models_test_ok = []
+		self.dist_test_ok = []
+		self.models_test_ko = []
+		self.dist_test_ko = []
+			
+		for ts in self.data_test_ok:
+			MOD1,MOD2 = FBAT.computeModels(ts,T,p,q,z)
+			self.models_test_ok.append([MOD1,MOD2])
+			distance = sum([(MOD2[i]-MOD1[i])**2 for i in range(len(MOD1))])**(0.5)
+			self.dist_test_ok.append(distance)
+			
+		for ts in self.data_test_ko:
+			MOD1,MOD2 = FBAT.computeModels(ts,T,p,q,z)
+			self.models_test_ko.append([MOD1,MOD2])
+			distance = sum([(MOD2[i]-MOD1[i])**2 for i in range(len(MOD1))])**(0.5)
+			self.dist_test_ko.append(distance)
+			
+		res = self.get_test_performance()
+		
+		stop = times.time()
+		self.timeTest = stop-start
+		
+		return res,self.threshold,self.timeTrain,self.timeTest
---
+++ main.py
@@ -0,0 +1,74 @@
+import sys,os
+import time as times
+import argparse
+from FBAT import FBAT
+
+
+
+def main(argv):
+
+	start = times.time()
+
+	parser=argparse.ArgumentParser()
+	parser.add_argument("--fileTrain",default=None,help="file with TRAIN TS")
+	parser.add_argument("--fileTest",default=None,help="file with TEST TS")
+	parser.add_argument("--fileOut",default=None,help="file out")
+	param=parser.parse_args()
+
+	fileTrain = param.fileTrain
+	fileTest = param.fileTest
+	fileOut = param.fileOut
+
+	TS_train_ok = []
+	TS_train_ko = []
+	TS_test_ok = []
+	TS_test_ko = []
+
+	#train
+	f = open(fileTrain,'r')
+	for line in f.readlines():
+		TS = [float(a) for a in line.split()[1:]]
+		if int(line.split()[0]) == 0:
+			TS_train_ok.append(TS)
+		if int(line.split()[0]) != 0:
+			TS_train_ko.append(TS)
+	f.close()
+	
+	#test
+	f = open(fileTest,'r')
+	for line in f.readlines():
+		TS = [float(a) for a in line.split()[1:]]
+		if int(line.split()[0]) == 0:
+			TS_test_ok.append(TS)
+		if int(line.split()[0]) != 0:
+			TS_test_ko.append(TS)
+#		if int(line.split()[0]) != 0:
+#			TS_test_ko.append(TS)
+	f.close()
+	
+
+	fbat = FBAT()
+	print("set train")
+	fbat.set_train(TS_train_ok, TS_train_ko)
+	print("set test")
+	fbat.set_test(TS_test_ok,TS_test_ko)
+	
+	print("fit train")
+	fbat.fit(p=24,q=12,z=1)
+	print("eval test")
+	results = fbat.eval_test(p=24,q=12,z=1)
+	
+	f = open(fileOut,'w')
+			
+	# accG,precG,recG,precOK,recOK,precKO,recKO
+	stop = times.time()
+	totalTime = stop-start
+	f.write("Z THRESHOLD TIME_TRAIN TIME_TEST TIME_TOTAL ACC PREC_TOT REC_TOT PREC_OK REC_OK PREC_KO REC_KO\n")
+	f.write("%d %f %f %f %f %f %f %f %f %f %f %f\n"%(1,results[1],results[2],results[3],totalTime,results[0][0],results[0][1],results[0][2],results[0][3],results[0][4],results[0][5],results[0][6]))
+	print("%d %f %f %f %f %f %f %f %f %f %f %f\n"%(1,results[1],results[2],results[3],totalTime,results[0][0],results[0][1],results[0][2],results[0][3],results[0][4],results[0][5],results[0][6]))
+	f.close()
+	
+	
+	
+if __name__ == '__main__':
+	main(sys.argv[1:])
